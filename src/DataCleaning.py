4ea45de96e48777cff69ada306531b2c783c793d
haowei772 <wehao@hotmail.com>
2017-05-15 15:55:11 -0700

    modify DataCleaning

--- src/DataCleaning.py
+++ src/DataCleaning.py
@@ -2,33 +2,26 @@ import pandas as pd
 import numpy as np
 from collections import defaultdict
 
-'''$$$$$$$$$$$$$$$$$$$$ Start Cleaning Data $$$$$$$$$$$$$$$$$$$$'''
 def clean_data(dft, folder):
-    '''remove and save the cases that are not closed'''
-    dft_still_open = dft[dft['Closed'].isnull()] # cases that not closed
-    filename_open = 'SF311_still_open_raw.csv'
-    dft_still_open_csv_path = folder + filename_open
-    dft_still_open.to_csv(dft_still_open_csv_path) # dft_still_open.csv contains cases that not closed
-
-    '''calculate the process time '''
+    '''
+    Clean data by removing cases that are not closed, closed before opened, or duplictaed.
+    Write the clean dataframe to a scv file.
+    INPUT: dft - a pandas dataframe, folder - a string of output loction
+    OUTPUT: a dataframe
+    '''
+    ''' remove cases that are not closed'''
     condition = dft['Closed'].notnull()
     dft_closed = dft[condition] # cases that are closed
+
+    '''calculate the process time in days and hours'''
     dft_closed['Process_days'] = dft_closed['Closed'] - dft_closed['Opened']
     dft_closed['Process_hours'] = dft_closed['Process_days'].apply(days_to_hours)
 
-    ''' remove cases with process time <= 0 hours and save theses cases '''
-    dft_wrong_dates = dft_closed[dft_closed['Process_hours'] <= 0]
-    filename_wrong_dates = 'SF311_wrong_dates_raw.csv'
-    dft_wrong_dates_csv_path = folder + filename_wrong_dates
-    dft_wrong_dates.to_csv(dft_wrong_dates_csv_path)
-    # dft_wrong_dates_raw.csv contains cases that have wrong dates: closed before opened'''re
+    ''' only keep cases that are closed after opened '''
     dft_right_dates = dft_closed[dft_closed['Process_hours'] > 0]
 
-    ''' remove duplicated cases and save theses cases '''
+    ''' remove duplicated cases '''
     dft_duplicates, dft_valid = check_word_in_col(dft_right_dates, 'Status Notes', 'Duplicate')
-    filename_duplicates = 'SF311_duplicates_raw.csv'
-    dft_duplicates_csv_path = folder + filename_duplicates
-    dft_duplicates.to_csv(dft_duplicates_csv_path) # dft_duplicates_raw.csv contains cases that are duplicated
 
     '''convert Process_days to float'''
     dft_valid['Process_days'] = dft_valid['Process_hours']/24.0
@@ -44,49 +37,19 @@ def clean_data(dft, folder):
     filename_reduced = 'SF311_valid_reduced.csv'
     dft_valid_reduced_csv_path = folder + filename_reduced
     dft_valid_reduced.to_csv(dft_valid_reduced_csv_path)
-
-    print 'Number of original cases: ', len(dft)
-    print 'Cases that are not closed: ', len(dft_still_open)
-    print 'Cases with process time <= 0: ', len(dft_wrong_dates)
-    print 'Cases with process time > 0: ', len(dft_right_dates)
-    print 'Duplicated cases: ', len(dft_duplicates)
-    print 'Valid cases: ', len(dft_valid)
     return dft_valid_reduced
 
-'''######## this can be a short lambda expression########'''
-def get_str_list(string):
-    '''convert to a list of string'''
-    return str(string).split()
-
-'''######## this can be a short lambda expression########'''
-def check_dup(str_list):
-    '''check if word 'Duplicate' is in the string list'''
-    return 'Duplicate' in str_list
-
-def check_word_in_col(df, column, word):
-    '''check if a word in the column, returns a tuple of dataframes,
-    the first one contains the word and second one does not'''
+def check_word_in_col(df, column, word='Duplicate'):
+    '''
+    Check if a specific word (e.g.,'Duplicate') is in the column of an entry, returns a dataframe
+    that contains the entries with the word.
+    INPUT: df - a pandas dataframe, column - string of the column name, word - string of a specific word
+    OUTPUT: a dataframe that contains the entries with the word
+    '''
     df1 = df.copy()
-    get_str_list = lambda x: str(x).split()
+    get_str_list = lambda x: str(x).split() # convert a string to a list of string
     check_dup = lambda x: word in x
 
     df1[column+'1'] = df1[column].apply(get_str_list) # turn df1[column] into a list of strings
     cond = df1[column+'1'].apply(check_dup) # check if df1[column+'1'] contains the word
-    df_found = df[cond]
-    df_not_found = df[~cond]
-    return df_found, df_not_found
-
-def creat_data_chunk(df, folder, filename, days = 730):
-    if timedelta(days = days) > df['Opened'].max()-df['Opened'].min():
-        print 'Error! Days over the limit!'
-        return
-    '''get the date that is 2 year before the data collecting date'''
-    new_end_date = df['Opened'].max() - timedelta(days = days)
-    condition1 = df['Opened'] <= new_end_date
-    condition2 = df['Process_days'] <= days
-    df_chunk = df[condition1 & condition2]
-    df_chunk.to_csv(folder+filename)
-    return
-
-
-'''$$$$$$$$$$$$$$$$$$$$ End Cleaning Data $$$$$$$$$$$$$$$$$$$$'''
+    return df[cond]
