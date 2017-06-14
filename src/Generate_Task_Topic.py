3f18834069612eddc866c47dfc55f550359868ff
haowei772 <wehao@hotmail.com>
2017-06-13 19:27:52 -0700

    machine learning models cleaned

--- src/Generate_Task_Topic.py
+++ src/Generate_Task_Topic.py
@@ -13,30 +13,20 @@ from FeatureEngineering import add_current_open
 if __name__ == '__main__':
 
 
-    # filename_engineer = '../data/SF311_engineered.csv'
-    # filename_engineer_new = '../data/SF311_engineered_open_case.csv'
-    # df_engi = EDA.get_prep_data(filename_engineer)
-    # print 'add features ...'
-    # df_engi = add_current_open(df_engi)
-    # de_engi.to_csv()
-
-
     '''Check if there is pickle files of dataframe and category dictionaries ready for load'''
     df_pickle_filename = '../data/df_tr.pickle'
     dict_pickle_filename = '../data/category_dict.pickle'
-    #rfr_model_pickle_filename = '../data/rfr_model.pickle'
     filename_train = '../data/SF311_train.csv'
 
-    #df_tr, category_dictionaries = get_df_for_modeling_topic(filename_train)
     df_tr, category_dictionaries = get_df_for_modeling(df_pickle_filename, dict_pickle_filename,filename_train)
 
     df_train, df_test = train_test_df_split(df_tr, test_size = 0.2, random_seed = 111)
     print 'df_train: ', len(df_train), ' df_test: ', len(df_test)
-    print df_train.head(1)
 
-    #target_col = 'Request Topic'
+    '''Generate 100 cluster of 'Request Topic''''
     target_col = 'Request Topic'
     nlpp = NLPProcessor(100)
+
     print 'fitting kmeans...'
     nlpp.fit_transform(df_train, target_col)
 
@@ -46,6 +36,7 @@ if __name__ == '__main__':
     kmeans_labels_test = nlpp.transform(df_test, target_col)
     print df_train.head(10)
     print 'kmeans_labels_train: ', kmeans_labels_train[:10]
+
     print '***************************'
     print df_test.head(10)
     print 'kmeans_labels_test: ', kmeans_labels_test[:10]
@@ -59,24 +50,3 @@ if __name__ == '__main__':
     df_test_kmeans_filename = '../data/df_test_kmeans.csv'
     df_train.to_csv(df_train_kmeans_filename)
     df_test.to_csv(df_test_kmeans_filename)
-
-
-
-    # X_train, y_train = get_X_y(df_train, 'Process_days')
-    # X_test, y_test = get_X_y(df_test, 'Process_days')
-    # print 'X_train: ', len(X_train)
-    # print 'X_test: ', len(X_test)
-    # rfr_model, yhat_randomF, randomF_rscore, oob_score = RandomForestRegressor_model(X_train, X_test, y_train, y_test, 100)
-    #
-    # mse_randomF = mean_squared_error(y_test, yhat_randomF)
-    #
-    # print 'randomF_rscore: ',randomF_rscore
-    # print 'oob_score: ',oob_score
-    # print 'mse_randomF: ', mse_randomF
-    # '''get training score'''
-    # yhat_train = rfr_model.predict(X_train)
-    # print 'training r2_score: ',r2_score(y_train, yhat_train)
-    #
-
-    # '''save the model to a pickle file'''
-    # dump_object_to_pickle(rfr_model, rfr_model_pickle_filename)
--- src/MachineLearningModel.py
+++ src/MachineLearningModel.py
@@ -8,88 +8,43 @@ from sklearn.metrics import mean_squared_error
 from EDA import days_to_hours
 from sklearn.metrics import r2_score
 import pickle
-# from matplotlib.colors import colorConverter
 
 
-
-'''$$$$$$$$$$$$$$$$$$$$ Start modeling $$$$$$$$$$$$$$$$$$$$'''
-'''linear regression model'''
 def LinearRegression_modle(X_train, X_test, y_train, y_test):
+    '''
+    Build a Linear regression model
+    INPUT: dataframe and series of X_train, y_train, X_test, y_test from train_test_split
+    OUTPUT: fitted model object, prediction based on X_test, test R2 score and MSE
+    '''
     regr = LinearRegression(copy_X=True, fit_intercept=True, n_jobs=3, normalize=False)
-
     regr.fit(X_train, y_train)
     r_square = regr.score(X_test, y_test)
-
     yhat_linear = regr.predict(X_test)
     mse_linear = mean_squared_error(y_test, yhat_linear)
-
     return regr, yhat_linear, r_square, mse_linear
 
-
-
-'''RandomForest regressor model'''
-'''RandomForestRegressor'''
 def RandomForestRegressor_model(X_train, X_test, y_train, y_test, estimators = 500):
-    rfr = RandomForestRegressor(n_estimators=estimators, criterion='mse', n_jobs=-2, oob_score=True)
-    '''other parameters:
-    RandomForestClassifier(n_estimators=10, criterion='mse', max_depth=None, min_samples_split=2,
-    min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features='auto', max_leaf_nodes=None,
-    min_impurity_split=1e-07, bootstrap=True, oob_score=False, n_jobs=1, random_state=None,
-    verbose=0, warm_start=False)
     '''
+    Build a random forest regressor model
+    INPUT: dataframe and series of X_train, X_test, y_train, X_test from train_test_split, number of trees
+    OUTPUT: fitted model object, prediction based on X_test, test R2 score and oob_score
+    '''
+    rfr = RandomForestRegressor(n_estimators=estimators, criterion='mse', n_jobs=-2, oob_score=True)
     rfr.fit(X_train, y_train)
-    '''r_square'''
     score = rfr.score(X_test,y_test)
     oob_score = rfr.oob_score_
     yhat = rfr.predict(X_test)
     return rfr, yhat, score, oob_score
 
-'''Gradient boosting model'''
-
-
 '''GradientBoostingRegressor'''
 def GradientBoostingRegressor_model(X_train, X_test, y_train, y_test, estimators = 200, learning_r = 0.1):
-    gbr = GradientBoostingRegressor(n_estimators=estimators, max_depth=3, subsample=1, learning_rate=learning_r)
-    '''other parameter:
-    GradientBoostingRegressor(loss='ls', learning_rate=0.1, n_estimators=100, subsample=1.0,
-    criterion='friedman_mse', min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0,
-    max_depth=3, min_impurity_split=1e-07, init=None, random_state=None, max_features=None,
-    alpha=0.9, verbose=0, max_leaf_nodes=None, warm_start=False, presort='auto')
     '''
+    Build a Gradient Boosting regressor model
+    INPUT: dataframe and series of X_train, X_test, y_train, X_test from train_test_split, number of estimators, learning rate
+    OUTPUT: fitted model object, prediction based on X_test, test R2 score
+    '''
+    gbr = GradientBoostingRegressor(n_estimators=estimators, max_depth=3, subsample=1, learning_rate=learning_r)
     gbr.fit(X_train, y_train)
-    '''r_square'''
     score = gbr.score(X_test,y_test)
     yhat = gbr.predict(X_test)
     return gbr, yhat, score
-
-# def plot_importance(model, X, max_features=10):
-#     '''Plot feature importance'''
-#     feature_importance = model.feature_importances_
-#     '''make importances relative to max importance'''
-#     feature_importance = 100.0 * (feature_importance / feature_importance.max())
-#     sorted_idx = np.argsort(feature_importance)
-#     pos = np.arange(sorted_idx.shape[0]) + .5
-#
-#     '''Show only top features'''
-#     pos = pos[-max_features:]
-#     feature_importance = (feature_importance[sorted_idx])[-max_features:]
-#     feature_names = (X.columns[sorted_idx])[-max_features:]
-#
-#     plt.figure(figsize = (12,8))
-#     plt.barh(pos, feature_importance, align='center', color = 'g')
-#     plt.yticks(pos, feature_names)
-#     plt.xlabel('Relative Importance')
-#     plt.title('Feature Importance')
-#     return feature_importance
-
-'''Will use KNN to impute the neighborhood, but before doing it need to split the data into train-valid and test set'''
-'''Because KNN will use information from the whole dataset, lead to a data leakage'''
-'''Do a 80-20% train-test split on dataframe'''
-# np.random.seed(seed = 111)
-# df_fill['Flag'] = np.random.random(size = len(df_fill)) >=0.8
-# df_train = df_fill[~df_fill['Flag']]
-# df_test = df_fill[df_fill['Flag']]
-
-
-
-'''$$$$$$$$$$$$$$$$$$$$ End modeling $$$$$$$$$$$$$$$$$$$$'''
